bdfd6d8873f504dd461f2c13a7e6ed384176c9ca
tk1024 <tk1024@users.noreply.github.com>
2021-12-09 00:02:54 +0900

    グローバルからAudioContextを外す

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,7 +53,6 @@ const config = {
   },
   plugins: [
     new webpack.ProvidePlugin({
-      AudioContext: ['standardized-audio-context', 'AudioContext'],
       Buffer: ['buffer', 'Buffer'],
     }),
     new webpack.EnvironmentPlugin({
