dfe50c979ac139c72be0cb3b61c57c2616989c76
tk1024 <tk1024@users.noreply.github.com>
2021-12-08 17:21:42 +0900

    クライアント2^18乗処理の削除

--- client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
+++ client/src/components/foundation/InfiniteScroll/InfiniteScroll.jsx
@@ -16,9 +16,7 @@ const InfiniteScroll = ({ children, fetchMore, items }) => {
   React.useEffect(() => {
     const handler = () => {
       // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
-        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      }).every(Boolean);
+      const hasReached = window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
 
       // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
       if (hasReached && !prevReachedRef.current) {
@@ -35,10 +33,10 @@ const InfiniteScroll = ({ children, fetchMore, items }) => {
     prevReachedRef.current = false;
     handler();
 
-    document.addEventListener('wheel', handler, { passive: false });
-    document.addEventListener('touchmove', handler, { passive: false });
-    document.addEventListener('resize', handler, { passive: false });
-    document.addEventListener('scroll', handler, { passive: false });
+    document.addEventListener('wheel', handler, { passive: true });
+    document.addEventListener('touchmove', handler, { passive: true });
+    document.addEventListener('resize', handler, { passive: true });
+    document.addEventListener('scroll', handler, { passive: true });
     return () => {
       document.removeEventListener('wheel', handler);
       document.removeEventListener('touchmove', handler);
