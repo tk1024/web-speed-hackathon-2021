2482b3f8e988afa3540c72d7c327c612381b6254
tk1024 <tk1024@users.noreply.github.com>
2021-12-13 03:45:35 +0900

    test

--- client/src/containers/TimelineContainer/TimelineContainer.tsx
+++ client/src/containers/TimelineContainer/TimelineContainer.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-
 import { InfiniteScroll } from '../../components/foundation/InfiniteScroll';
 import { TimelinePage } from '../../components/timeline/TimelinePage';
 import { useInfiniteFetch } from '../../hooks/use_infinite_fetch';
 import { fetchJSON } from '../../utils/fetchers';
 
-/** @type {React.VFC} */
-const TimelineContainer = () => {
-  const { data: posts, fetchMore } = useInfiniteFetch('/api/v1/posts', fetchJSON);
+interface Props {
+  posts: any[]
+}
+
+const TimelineContainer = (props: Props) => {
+  const { data: posts, fetchMore } = useInfiniteFetch('/api/v1/posts', fetchJSON, props.posts);
 
   return (
     <InfiniteScroll fetchMore={fetchMore} items={posts}>
