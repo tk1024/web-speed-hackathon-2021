5242a3ef3d37b231bf16dab5fbce832a4b27dd50
tk1024 <tk1024@users.noreply.github.com>
2021-12-13 21:54:41 +0900

    tmp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,11 @@
 import type { AppProps } from 'next/app';
 import React, { useEffect } from 'react';
 import { AppPage } from '../client/src/components/application/AppPage';
-import '../styles/globals.css';
-// import '../styles/webfont.css';
-// import 'tailwindcss/tailwind.css'
-
 import { AuthModalContainer } from '../client/src/containers/AuthModalContainer';
 import { NewPostModalContainer } from '../client/src/containers/NewPostModalContainer';
 import { useFetch } from '../client/src/hooks/use_fetch';
 import { fetchJSON } from '../client/src/utils/fetchers';
+import '../styles/globals.css';
 
 /** @type {React.VFC} */
 function MyApp({ Component, pageProps }: AppProps) {
@@ -20,9 +17,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   }, [data])
 
   useEffect(() => {
-    setTimeout(() => {
-      document.head.innerHTML += `<link rel="stylesheet" href="webfont.css" />`
-    }, 100)
+    document.head.innerHTML += `<link rel="stylesheet" href="webfont.css" />`
   }, [])
 
   const [modalType, setModalType] = React.useState('none');
