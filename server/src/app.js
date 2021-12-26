e14d1992f11d31057346420df4717d1be297ed33
tk1024 <tk1024@users.noreply.github.com>
2021-12-27 02:49:44 +0900

    サーバーの圧縮有効化

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import Express from 'express';
 import session from 'express-session';
+import compression from 'compression';
 
 import { apiRouter } from './routes/api';
 import { staticRouter } from './routes/static';
@@ -17,16 +18,9 @@ app.use(
     secret: 'secret',
   }),
 );
+app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.raw({ limit: '10mb' }));
-
-app.use((_req, res, next) => {
-  res.header({
-    'Cache-Control': 'max-age=0, no-transform',
-    Connection: 'close',
-  });
-  return next();
-});
 
 app.use('/api/v1', apiRouter);
 app.use(staticRouter);
