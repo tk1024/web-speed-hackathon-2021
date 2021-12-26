46fc540883b46ac6d5e43728f819026c52c28dc5
tk1024 <tk1024@users.noreply.github.com>
2021-12-26 23:52:08 +0900

    JPEGの代わりにAVI画像を表示する

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -1,5 +1,4 @@
 import classNames from 'classnames';
-import sizeOf from 'image-size';
 import React from 'react';
 
 import { useFetch } from '../../../hooks/use_fetch';
@@ -16,15 +15,15 @@ import { fetchBinary } from '../../../utils/fetchers';
  * @type {React.VFC<Props>}
  */
 const CoveredImage = ({ alt, src }) => {
-  const { data, isLoading } = useFetch(src, fetchBinary);
+  const [imageSize, setImageSize] = React.useState(null)
 
-  const imageSize = React.useMemo(() => {
-    return data !== null ? sizeOf(Buffer.from(data)) : null;
-  }, [data]);
-
-  const blobUrl = React.useMemo(() => {
-    return data !== null ? URL.createObjectURL(new Blob([data])) : null;
-  }, [data]);
+  React.useEffect(() => {
+    const img = new Image()
+    img.onload = function () {
+      setImageSize({ width: this.width, height: this.height })
+    }
+    img.src = src
+  }, [src, setImageSize]);
 
   const [containerSize, setContainerSize] = React.useState({ height: 0, width: 0 });
   /** @type {React.RefCallback<HTMLDivElement>} */
@@ -35,12 +34,12 @@ const CoveredImage = ({ alt, src }) => {
     });
   }, []);
 
-  if (isLoading || data === null || blobUrl === null) {
+  if (!imageSize) {
     return null;
   }
 
   const containerRatio = containerSize.height / containerSize.width;
-  const imageRatio = imageSize?.height / imageSize?.width;
+  const imageRatio = imageSize.height / imageSize.width;
 
   return (
     <div ref={callbackRef} className="relative w-full h-full overflow-hidden">
@@ -50,7 +49,9 @@ const CoveredImage = ({ alt, src }) => {
           'w-auto h-full': containerRatio > imageRatio,
           'w-full h-auto': containerRatio <= imageRatio,
         })}
-        src={blobUrl}
+        width={imageSize.width}
+        height={imageSize.height}
+        src={src}
       />
     </div>
   );
