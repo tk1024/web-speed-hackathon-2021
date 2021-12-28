2c1b0d91c1f228a8b9175353929a43404b8bf719
tk1024 <tk1024@users.noreply.github.com>
2021-12-29 05:33:41 +0900

    React読込タイミングの更新

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
 import { AppContainer } from './containers/AppContainer';
 
-window.addEventListener('load', () => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <AppContainer />
-    </BrowserRouter>,
-    document.getElementById('app'),
-  );
-});
+ReactDOM.render(
+  <BrowserRouter>
+    <AppContainer />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
